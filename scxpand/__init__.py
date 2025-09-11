7d3da4f72ea8fdd8a48db8fcd38a20ef8bb0c6ed
ronamit <ronamit@campus.technion.ac.il>
2025-09-11 17:58:19 +0300

    add version and list_pretrained

--- scxpand/__init__.py
+++ scxpand/__init__.py
@@ -4,13 +4,11 @@ A framework for predicting T-cell clonal expansion from single-cell RNA sequenci
 using multiple machine learning approaches including autoencoders, MLPs, LightGBM, and linear models.
 """
 
-# Import core functionality
+from importlib.metadata import version
+
 from scxpand.core.inference import run_inference
 from scxpand.core.prediction import run_prediction_pipeline
-
-# Import CLI functions (lazy import to avoid module loading conflicts)
-# from scxpand.main import list_pretrained_models
-# Import pre-trained model functionality
+from scxpand.main import list_pretrained_models
 from scxpand.pretrained import (
     PRETRAINED_MODELS,
     download_pretrained_model,
@@ -19,11 +17,16 @@ from scxpand.pretrained import (
 from scxpand.util.classes import ModelType
 
 
+__version__ = version("scxpand")
+_version_ = __version__  # Alias for compatibility
+
 __all__ = [
     "PRETRAINED_MODELS",
     "ModelType",
+    "_version_",
     "download_pretrained_model",
     "get_pretrained_model_info",
+    "list_pretrained_models",
     "run_inference",
     "run_prediction_pipeline",
 ]
